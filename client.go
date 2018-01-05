f6acbb1d379836c4139886ffd928f20c372d8f63
Zhongren Shao <shao.zhongren@gmail.com>
2018-01-04 21:10:34 -0600

    Added postgres.

--- client.go
+++ client.go
@@ -1,49 +1,61 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"os"
-	"strings"
 
-	"github.com/pdepip/go-binance/binance"
+	_ "github.com/lib/pq"
 )
 
 func main() {
-
-	client := binance.New(os.Getenv("BINANCE_API"), os.Getenv("BINANCE_SECRET"))
-	positions, err := client.GetPositions()
-
+	connStr := fmt.Sprintf(
+		"postgres://%s:%s@localhost/shaoz?sslmode=disable",
+		os.Getenv("PG_USER"),
+		os.Getenv("PG_PASS"))
+	db, err := sql.Open("postgres", connStr)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
+	rows, err := db.Exec("CREATE TABLE test(id integer);")
+	fmt.Println(rows, err)
+	defer db.Close()
 
-	for _, p := range positions {
-		symbol := strings.Join([]string{
-			p.Asset,
-			"BTC",
-		}, "")
-		query := binance.SymbolQuery{
-			Symbol: symbol,
-		}
+	// client := binance.NewClient(os.Getenv("BINANCE_API"), os.Getenv("BINANCE_SECRET"))
 
-		res, err := client.GetLastPrice(query)
+	// res, err := client.NewGetAccountService().Do(context.Background())
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// for i, v := range res.Balances {
+	// 	fmt.Println(i, v)
+	// }
 
-		if err != nil {
-			panic(err)
-		}
+	// wsDepthHandler := func(event *binance.WsDepthEvent) {
+	// 	fmt.Println(event)
+	// }
+	// done, err := binance.WsDepthServe("LTCBTC", wsDepthHandler)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// <-done
 
-		fmt.Println(p.Asset, res)
-	}
-
-	query := binance.SymbolQuery{
-		Symbol: "123456",
-	}
-
-	res, err := client.GetLastPrice(query)
-
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(res)
+	// 	depthRes, err := client.NewDepthService().Symbol("XLMBTC").
+	// 		Do(context.Background())
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 		return
+	// 	}
+	// 	for _, v := range depthRes.Bids {
+	// 		fmt.Println(v)
+	// 		// largest to smallest limit buy
+	// 	}
+	// 	fmt.Println("--------------------------------------------------------------------------------")
+	// 	for _, v := range depthRes.Asks {
+	// 		fmt.Println(v)
+	// 		// smallest to largest limit sell
+	// 	}
 }
